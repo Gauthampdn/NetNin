c4179468f19fe4f6e627ad0f4ca713d21899c83f
Gautham P <gauthampdn@gmail.com>
2023-04-06 22:32:05 -0700

    moved folder and edits

--- nn/src/App.js
+++ nn/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './NavBar'
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <div className="content">
+       <Home/>
+      </div>
     </div>
   );
 }
