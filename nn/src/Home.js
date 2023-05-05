7ffd92d90c9318278ce626130c7ea2f891d4e1c9
Gautham P <gauthampdn@gmail.com>
2023-05-05 12:48:48 -0700

    New changes, and delete hi

--- nn/src/Home.js
+++ nn/src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("hello Ninjas");
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>click me</button>
         </div>
     );
 }
