a7c1048466bcc70b135d611403c81698ae27898a
Gautham P <gauthampdn@gmail.com>
2023-05-05 12:53:09 -0700

    all changes

--- nn/src/Home.js
+++ nn/src/Home.js
@@ -1,12 +1,19 @@
-const Home = () => {
+import {useState} from 'react';
 
+const Home = () => {
+    //making the initial val = mario
+    const[name, setName] = useState('mario');
+    const[age, setAge] = useState(25)
+    
     const handleClick = () => {
-        console.log("hello Ninjas");
+        setName('luigi');
     }
+
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>click me</button>
         </div>
     );
