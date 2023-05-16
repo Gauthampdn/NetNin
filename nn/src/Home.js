e3298de4b2e098193bc7d39bf02feeb514b1d0b3
gauthampdn <gauthampdn@gmail.com>
2023-05-15 17:40:59 -0700

    json

--- nn/src/Home.js
+++ nn/src/Home.js
@@ -1,21 +1,22 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import People from './People';
 
 
  
 const Home = () => {
     //making the initial val = mario
-    const[peeps, setPeeps] = useState([
-        {name: "Gautham", Age: 19, Major: "CS", id: 1},
-        {name: "bob", Age: 19, Major: "bio", id: 2},
-        {name: "sally", Age: 20, Major: "PS", id: 3}
+    const[peeps, setPeeps] = useState(null);
 
-    ]);
+
 
     const handleDelete = (id) => {
         const newPeeps = peeps.filter(individual => individual.id !== id)
         setPeeps(newPeeps)
     }
+
+    useEffect(() => {
+        fetch("http://localhost:8000/peeps")
+    }, []);
 
     return (
         <div className="home">
