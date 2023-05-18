1788704ca2c27bb59ced143671e3df7bbb3b7948
Gautham P <gauthampdn@gmail.com>
2023-05-17 21:16:22 -0700

    error handling

--- nn/src/Home.js
+++ nn/src/Home.js
@@ -7,33 +7,35 @@ const Home = () => {
     //making the initial val = mario
     const[peeps, setPeeps] = useState(null);
     const[loading, setloading] = useState(true);
-
-
-
-    const handleDelete = (id) => {
-        const newPeeps = peeps.filter(individual => individual.id !== id)
-        setPeeps(newPeeps)
-    }
+    const[error, setError] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:8000/peeps")
             .then( res => {
+                if(!res.ok){
+                    throw Error("couldn't fetch data from source")
+                }
                 return res.json();
             })
             .then(data => {
                 setTimeout(() => {
-                    setPeeps(data)
-                    setloading(false)
-
-
-                }, 500)
+                    setPeeps(data);
+                    setloading(false);
+                    setError(null)
+                }, 500);
+            })
+            .catch(err => {
+                console.log(err.message);
+                setError(err.message)
+                setloading(false)
             })
     }, []);
 
     return (
         <div className="home">
+            {error && <div> {error} </div> }
             {loading && <div> loading... </div>}
-            {peeps && <People peoples = {peeps} hd = {handleDelete}/>}
+            {peeps && <People peoples = {peeps} />}
         </div>
     );
 }
--- nn/src/People.js
+++ nn/src/People.js
@@ -1,7 +1,6 @@
 const People = (props) => {
 
     const peeps = props.peoples
-    const handleDelete = props.hd
 
     return (
         <div className="People-list">
@@ -10,7 +9,6 @@ const People = (props) => {
                 <div className="p-prev" key={person.id}>
                     <h2>{person.name}</h2>
                     <h3>My major is {person.Major} and I am {person.Age} years old!</h3>
-                    <button className="delBut" onClick={() => handleDelete(person.id)}> Delete </button>
                 </div>
 
             ))}
